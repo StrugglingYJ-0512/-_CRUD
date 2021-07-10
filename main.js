76c6ba763d51639c268fa03ef7746ee0425151bb
username <StrugglingYJ-0512.name>
2021-07-10 23:29:21 +0900

    1.npm  2. url을 통해서..
    1. nodenmon 설치하면서, 2개의 파일이 생성되어야 하는데.
    1) packageLock.json
    2) package.json
     이를 위해서, npm insall / npm init 하고 난 후에, npm install nodemon -g , npm start  라고 명렁어를  친다.
    2. 코드는 URL 을 통해서 입력된 값을 사용한다.

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // url 모듈을 사용 할 것이다. 
 
 
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == '/') {
-    url = '/index.html';
+  var _url = request.url; // url 에 언더바를 붙인 이유 ; 위의 url 모듈의 변수명이 url이어서 걍 바꿈.
+  var queryData = url.parse(_url, true).query;
+  console.log(queryData.id);
+  if (_url == '/') {
+    _url = '/index.html';
   }
-  if (request.url == '/favicon.ico') {
+  if (_url == '/favicon.ico') {
     response.writeHead(404);
     response.end();
     return;
   }
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+  // console.log(__dirname + _url);
+  //출력 : C:\Users\82107\Documents\GitHub\CRUD/index.html
+  //__dirname(폴더경로) ; C:\Users\82107\Documents\GitHub\CRUD 
+  // _url : index.html  
+  response.end(queryData.id);
+  // 사용자에게 전송하는 데이터 ; fs.readFileSync(__dirname + url)
+  // response.end('YJ' + url);
 
 });
 app.listen(3000);
