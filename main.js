a8c1b293560511e7d615e333cbc12425fda828be
username <StrugglingYJ-0512.name>
2021-07-13 16:17:51 +0900

    함수 이용해서 정리함.
    함수
    1. 글목록
    2. 본문( 특히, 본문의 argument 사용법잘 볼것!!)

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) { // 본문내용
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>                                     
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) { // 글목록 가져옴
+  var list = '<ul>';
+  for (var i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   console.log(url.parse(_url, true));
-  // console.log(url.parse(_url, true) 을 콘솔에 찍어보자. 
-  // 찍어보면, 그 안에 pathname 이 존재한다. 의미하는 바는 path는 queryString까지 모두 포함이고,
-  // pathname은 queryString을 제외한 값만 있음을 확인할 수 있다!!
-
 
   if (pathname === '/') { // 이렇게만 홑if문이라면, 최상위 루트인 홈페이지는 undefined상태이다.
     if (queryData.id === undefined) {// /만 존재하는 최상위루트 홈페이지 상태.
@@ -22,28 +44,11 @@ var app = http.createServer(function (request, response) {
         var description = 'Hello, Node.js';
 
         // 글 목록 가져옴. 
-        var list = '<ul>';
-        for (var i = 0; i < filelist.length; i++) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
-        }
-        list = list + '</ul>';
+        var list = templateList(filelist);
 
         // 본문
-        var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>                                     
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-          ${list}
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
         response.writeHead(200);
         response.end(template);
       })
@@ -51,30 +56,13 @@ var app = http.createServer(function (request, response) {
     } else {
       fs.readdir('./data', (err, filelist) => {
         // 글 목록 가져오기.
-        var list = '<ul>';
-        for (var i = 0; i < filelist.length; i++) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
-        }
-        list = list + '</ul>';
+        var list = templateList(filelist);
 
         // 본문
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>                                     
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
           response.writeHead(200);
           response.end(template);
         });
