952bdac410bb7a22d8e4f21d3eea845731f4f982
username <StrugglingYJ-0512.name>
2021-07-13 18:14:19 +0900

    What is Callback?
    callback 함수란, 함수의 매개인자로 받는 함수이다.
    따라서, 함수의 기능을 실행한 후에 실행하는 함수이다.

--- syntax/sync.js
+++ syntax/sync.js
@@ -14,11 +14,12 @@ console.log('------------------------------------------------')
 // 따라서, Nodejs는 비동기 실행을 선호한다.라는 뜻
 
 console.log('A');
-// readFileSync 는 return 값이 있다 ( 따라서, result 변수에 할당 가능)
-// readFile 은 return 값이 없다  ==> 대신, 콜백 함수를 3번째 인자로 줘야한다.
-// 파일을 읽는 작업 끝 ==> 3번째 인자인 함수를 실행하고, 
-// (err, result) ; 에러가 있다면, err 인자에 실패정보를 공급 / 
-//                 파일을 가져오는 작업이 성공했따면, result에 파일의 내용을 인자로써 공급
+/* readFileSync 는 return 값이 있다 ( 따라서, result 변수에 할당 가능)
+ readFile 은 return 값이 없다  ==> 대신, 콜백 함수를 3번째 인자로 줘야한다.
+  (err, result) ; 에러가 있다면, err 인자에 실패정보를 공급 / 
+ 파일을 읽는 작업 끝 ==> 3번째 인자인 함수를 실행하고, 
+                 파일을 가져오는 작업이 성공했따면, result에 파일의 내용을 인자로써 공급
+                 */
 fs.readFile('syntax/sample.txt', 'utf-8', (err, result) => {
   console.log(result);
 
