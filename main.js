48e80ff91717fa6bbd275fdda3353c26a287bc90
username <StrugglingYJ-0512.name>
2021-07-14 21:09:45 +0900

    update  화면
    1. esle if( pathname ==='/update') 로 update 화면 구성.
    2. 그 내용으로 form 을 가져온다.
    3. point > 구성 할 때, 수정하는 파일 이름은 보전되어야 한다!

--- main.js
+++ main.js
@@ -38,21 +38,16 @@ var app = http.createServer(function (request, response) {
 
   if (pathname === '/') {
     if (queryData.id === undefined) {// /만 존재하는 최상위루트 홈페이지 상태.
-
       fs.readdir('./data', (err, filelist) => {
         console.log(filelist);
-
         var title = 'Welcome';
         var description = 'Hello, Node.js';
-
         // 글 목록 가져옴. 
         var list = templateList(filelist);
-
         // 본문
         var template = templateHTML(title, list,
           `<h2>${title}</h2>${description}`,
           ` <a href="/create">create</a> `);
-
         response.writeHead(200);
         response.end(template);
       })
@@ -60,7 +55,6 @@ var app = http.createServer(function (request, response) {
     } else { // id 값이 존재함. ( 최상위 디렉터리가 아님)
       fs.readdir('./data', (err, filelist) => {
         var list = templateList(filelist);
-
         // 본문
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
@@ -73,16 +67,14 @@ var app = http.createServer(function (request, response) {
       });
     }
   } else if (pathname === '/create') {
-    fs.readdir('./data', (err, file list) => {
+    fs.readdir('./data', (err, filelist) => {
       console.log(filelist);
-
       var title = 'Web - create';
       // 글 목록 가져옴. 
       var list = templateList(filelist);
-
       // 본문
       var template = templateHTML(title, list, `
-      <form action="http://localhost:3000/create_process" method="post">
+      <form action="/create_process" method="post"> 
       <p><input type="text" name="title" placeholder="title"></p>
       <p>
         <textarea placeholder="description" name="description"></textarea>
@@ -92,15 +84,12 @@ var app = http.createServer(function (request, response) {
       </p>
     </form>
       `, '');
-
       response.writeHead(200);
       response.end(template);
     })
+
   } else if (pathname === '/create_process') {
-
     var body = '';
-
-
     request.on('data', (data) => {
       body += data;
     });
@@ -110,11 +99,7 @@ var app = http.createServer(function (request, response) {
       var title = post.title;
       var description = post.description;
       console.log(post.title);
-
-
-
       fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-
         response.writeHead(302,
           { Location: `/?id=${title}` });
         response.end();
@@ -122,15 +107,42 @@ var app = http.createServer(function (request, response) {
     });
 
 
+    /*  update 화면 
+        1. form  2.read기능 ; 수정할 data가 필요  
+    */
+    // 아래의 코드는 id값이 존재할때의 같은 코드이다(detail은 당근 수정!)
+  } else if (pathname === '/update') {
+    fs.readdir('./data', (err, filelist) => {
+      console.log('update 화면')
 
+      // 본문
+      fs.readdir('./data', (err, filelist) => {
+        var list = templateList(filelist);
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var title = queryData.id;
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post"> 
+            <input type="hidden" name="id" value="${title}">  
+            <p><input type="text" name="title" value="${title}" placeholder="title"></p>
+              <p>
+              <textarea placeholder="description" name="description">${description}</textarea>
+              </p>
+              <p>
+              <input type="submit" />
+              </p>
+            </form>
+            `,
+            `<a href="/create">create<a/> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    });
   } else {
     response.writeHead(404);
     response.end('Not Found');
-
   }
-
-
-
-
 });
+
 app.listen(3000);
